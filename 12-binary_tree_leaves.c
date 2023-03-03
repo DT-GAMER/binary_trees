10c855d357e34057017f28a131a03b1a8b914f3e
Abakpa Dominic <abakpad82@gmail.com>
2023-03-04 00:51:59 +0100

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,13 +1,9 @@
 #include "binary_trees.h"
 
 /**
-
  * binary_tree_leaves - counts the leaves in a tree
-
  *@tree: pointer to the tree
-
  * Return: number of leaves
-
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
