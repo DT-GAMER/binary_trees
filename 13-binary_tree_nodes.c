373320f225288e55c75d023726852231b74906df
Abakpa Dominic <abakpad82@gmail.com>
2023-03-04 00:52:57 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,13 +1,9 @@
 #include "binary_trees.h"
 
 /**
-
  * binary_tree_nodes - counts nodes with at least one child
-
  *@tree: the pointer to the tree
-
  * Return: the number of nodes
-
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
