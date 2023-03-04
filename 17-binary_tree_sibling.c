cc79489a9ecdc9d5dd19dd07cc9f84eb004225ad
Abakpa Dominic <abakpad82@gmail.com>
2023-03-04 06:31:00 +0100

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,17 +1,9 @@
 #include "binary_trees.h"
 
 /**
-
  * binary_tree_sibling - finds the sibling of a node
-
- *
-
  *@node: pointer to the node to find the sibling
-
- *
-
  * Return: a pointer to the sibling node else NULL
-
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
